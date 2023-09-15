e910d19b3d762553dd1be20047ceb06ead3e0db7
Алексей <87669945+grevold@users.noreply.github.com>
2023-09-15 16:46:04 +0300

    Создал базу приложения (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import s from "./index.module.css";
+import { Pad } from "./components/Pad/Pad";
+import { texts } from "./Texts";
+
 function App() {
-  return <h1>Hello, world!</h1>;
+  const arrayOfPads = texts.Main.pads;
+  console.log("render");
+  return (
+    <div className={s.root}>
+      <h1 className={s.header}>Drum Pads</h1>
+      <div className={s.playGround}>
+        {arrayOfPads.map((pad) => (
+          <Pad key={pad.sample} sound={pad.sample} color={pad.color} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
