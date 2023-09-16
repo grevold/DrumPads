aeffc243d3d33a525cf7b0fc0a6ae64d1713325e
Alexey Grevtsov <grevold@yandex.ru>
2023-09-16 23:05:31 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
+// import { Pad } from "./components/Pad/Pad";
+import { useEffect, useRef } from "react";
+// import { texts } from "./Texts";
+
 import s from "./index.module.css";
-import { Pad } from "./components/Pad/Pad";
-import { texts } from "./Texts";
 
 function App() {
-  const arrayOfPads = texts.Main.pads;
-  console.log("render");
-  return (
-    <div className={s.root}>
-      <h1 className={s.header}>Drum Pads</h1>
-      <div className={s.playGround}>
-        {arrayOfPads.map((pad) => (
-          <Pad key={pad.sample} sound={pad.sample} color={pad.color} />
-        ))}
-      </div>
-    </div>
+  // const arrayOfPads = texts.Main.pads;
+  // console.log("render");
+
+  const sample = useRef(
+    new Howl({
+      src: [`${process.env.PUBLIC_URL}/Samples/Kick_01.mp3`],
+    })
   );
+
+  useEffect(() => {
+    const handleClick = () => {
+      sample.current.play();
+    };
+    window.addEventListener("touchstart", handleClick);
+    return () => window.removeEventListener("touchstart", handleClick);
+  }, []);
+
+  return <h1>Hello, world!</h1>;
+
+  // return (
+  //   <div className={s.root}>
+  //     <h1 className={s.header}>Drum Pads</h1>
+  //     <div className={s.playGround}>
+  //       {arrayOfPads.map((pad) => (
+  //         <Pad key={pad.sample} sound={pad.sample} color={pad.color} />
+  //       ))}
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default App;
