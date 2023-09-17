c31f202fba604d186d732c4a9bc84c20606e160f
Alexey Grevtsov <grevold@yandex.ru>
2023-09-17 18:42:48 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,20 @@
-// import { Pad } from "./components/Pad/Pad";
-import { useEffect, useRef } from "react";
-// import { texts } from "./Texts";
-import { Howl } from "howler";
-// @ts-ignore: Unreachable code error
-import initReactFastclick from "react-fastclick";
+import { Pad } from "./components/Pad/Pad";
+import { texts } from "./Texts";
 
-// import s from "./index.module.css";
+import s from "./index.module.css";
 
 function App() {
-  // const arrayOfPads = texts.Main.pads;
-  // console.log("render");
-
-  useEffect(() => {
-    // console.log(attachFastClick)
-
-    initReactFastclick();
-  }, []);
-
-  const sample = useRef(
-    new Howl({
-      src: [`${process.env.PUBLIC_URL}/Samples/Kick_01.mp3`],
-    })
+  const arrayOfPads = texts.Main.pads;
+  return (
+    <div className={s.root}>
+      <h1 className={s.header}>Drum Pads</h1>
+      <div className={s.playGround}>
+        {arrayOfPads.map((pad) => (
+          <Pad key={pad.sample} sound={pad.sample} color={pad.color} />
+        ))}
+      </div>
+    </div>
   );
-
-  useEffect(() => {
-    const handleClick = () => {
-      sample.current.play();
-    };
-    window.addEventListener("touchstart", handleClick);
-    return () => window.removeEventListener("touchstart", handleClick);
-  }, []);
-
-  return <h1>Stieve Rambo</h1>;
-
-  // return (
-  //   <div className={s.root}>
-  //     <h1 className={s.header}>Drum Pads</h1>
-  //     <div className={s.playGround}>
-  //       {arrayOfPads.map((pad) => (
-  //         <Pad key={pad.sample} sound={pad.sample} color={pad.color} />
-  //       ))}
-  //     </div>
-  //   </div>
-  // );
 }
 
 export default App;
--- src/components/Pad/Pad.tsx
+++ src/components/Pad/Pad.tsx
@@ -1,5 +1,8 @@
+import { useEffect, useRef, useCallback } from "react";
+import * as Tone from "tone";
+import { Sampler } from "tone";
+
 import s from "./Pad.module.css";
-import { Howl } from "howler";
 
 interface Props {
   sound: string;
@@ -7,16 +10,39 @@ interface Props {
 }
 
 export function Pad({ sound, color }: Props) {
-  const sample = new Howl({
-    src: [`${process.env.PUBLIC_URL}/Samples/${sound}`],
-  });
+  // const sample = new Howl({
+  //   src: [`${process.env.PUBLIC_URL}/Samples/${sound}`],
+  // });
+  const playerRef = useRef<Tone.Sampler>();
+
+  const handleClick = useCallback(() => {
+    const player = playerRef.current;
+    if (player?.loaded) {
+      player.triggerAttack("C4");
+    } else {
+      console.log("Звук ещё не загрузился!");
+    }
+  }, []);
+
+  useEffect(() => {
+    const player = new Sampler({
+      urls: {
+        C4: "C4.mp3",
+      },
+      baseUrl: "https://tonejs.github.io/audio/salamander/",
+    }).toDestination();
+    // const pingPong = new Tone.PingPongDelay("1n", 0.3).toDestination();
+    // const pitch = new Tone.PitchShift(10).toDestination();
+    // player.connect(pingPong);
+    // player.connect(pitch);
+    playerRef.current = player;
+  }, [sound]);
+
   return (
     <button
       className={s.root}
       style={{ backgroundColor: color }}
-      onTouchStart={() => {
-        sample.play();
-      }}
+      onTouchStart={handleClick}
     ></button>
   );
 }
