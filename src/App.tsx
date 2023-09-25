ab761d27849ba69ae992a2317a22ad640bd64f3c
Алексей <87669945+grevold@users.noreply.github.com>
2023-09-25 14:26:20 +0300

    Загрузил новые звуки (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,48 @@
 import { Pad } from "./components/Pad/Pad";
 import { texts } from "./Texts";
+import { useState } from "react";
 
 import s from "./index.module.css";
 
 function App() {
   const arrayOfPads = texts.Main.pads;
+  const [state, setState] = useState(false);
+  const [isPingPongMode, setPingPongMode] = useState(false);
+
+  if (!state) {
+    return (
+      <button
+        onClick={() => {
+          setState(true);
+        }}
+      >
+        Начать
+      </button>
+    );
+  }
+
   return (
     <div className={s.root}>
       <h1 className={s.header}>Drum Pads</h1>
       <div className={s.playGround}>
-        {arrayOfPads.map((pad) => (
-          <Pad key={pad.sample} sound={pad.sample} color={pad.color} />
+        {arrayOfPads.Classic_Hip_Hop.A.map((pad) => (
+          <Pad
+            key={pad.sample}
+            soundPack={"Classic_Hip_Hop"}
+            bank={"A"}
+            sound={pad.sample}
+            color={pad.color}
+            isPingPongMode={isPingPongMode}
+          />
         ))}
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          checked={isPingPongMode}
+          onChange={() => setPingPongMode((value) => !value)}
+        />
+        <label>Ping-pong mode</label>
       </div>
     </div>
   );
