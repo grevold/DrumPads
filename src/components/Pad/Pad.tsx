3ef32ba28869180e25e889b97066338eb9e3683c
Алексей <87669945+grevold@users.noreply.github.com>
2023-09-25 18:15:07 +0300

    добавил блок эффектов (#5)

--- src/components/Pad/Pad.tsx
+++ src/components/Pad/Pad.tsx
@@ -9,12 +9,28 @@ interface Props {
   color: string;
   soundPack: string;
   bank: string;
-  isPingPongMode?: boolean;
+  isPingPongMode: boolean;
+  isPhaserMode: boolean;
+  isCrusherMode: boolean;
 }
 
-const pingPong = new Tone.PingPongDelay("4n", 0.5).toDestination();
+const pingPong = new Tone.PingPongDelay("8n", 0.5).toDestination();
+const phaser = new Tone.Phaser({
+  frequency: 10,
+  octaves: 2,
+  baseFrequency: 1000,
+}).toDestination();
+const crusher = new Tone.BitCrusher(4).toDestination();
 
-export function Pad({ sound, color, isPingPongMode, soundPack, bank }: Props) {
+export function Pad({
+  sound,
+  color,
+  isPingPongMode,
+  isPhaserMode,
+  isCrusherMode,
+  soundPack,
+  bank,
+}: Props) {
   const playerRef = useRef<Tone.Player>();
   const handleClick = useCallback(() => {
     const player = playerRef.current;
@@ -34,16 +50,22 @@ export function Pad({ sound, color, isPingPongMode, soundPack, bank }: Props) {
   }, [sound]);
 
   useEffect(() => {
-    const player = playerRef.current;
-    if (!player) return;
-    if (isPingPongMode) {
-      player.connect(pingPong);
-    } else {
-      playerRef.current = new Player(
-        `${process.env.PUBLIC_URL}/Samples/${soundPack}/${bank}/${sound}`
-      ).toDestination();
+    playerRef.current = new Player(
+      `${process.env.PUBLIC_URL}/Samples/${soundPack}/${bank}/${sound}`
+    ).toDestination();
+
+    if (isPhaserMode) {
+      playerRef.current.connect(phaser);
     }
-  }, [isPingPongMode]);
+
+    if (isPingPongMode) {
+      playerRef.current.connect(pingPong);
+    }
+
+    if (isCrusherMode) {
+      playerRef.current.connect(crusher);
+    }
+  }, [isPingPongMode, isPhaserMode, isCrusherMode]);
 
   return (
     <button
