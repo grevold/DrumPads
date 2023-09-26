870d2ec27607b86d50a1b5f9bede98e5b3d41965
Алексей <87669945+grevold@users.noreply.github.com>
2023-09-27 00:26:48 +0300

    Добавил селект паков (#8)

--- src/components/Header/ModalMenu/ModalMenu.tsx
+++ src/components/Header/ModalMenu/ModalMenu.tsx
@@ -1,9 +1,37 @@
+import { Select } from "antd";
 import s from "./ModalMenu.module.css";
+import {
+  EPack,
+  soundEffectsActions,
+} from "../../../store/soundEffectsConfigSlice";
+import { useCallback } from "react";
+import { useAppDispatch, useAppSelector } from "../../../store/store";
 
 interface Props {
   isOpened: boolean;
 }
 
 export function ModalMenu({ isOpened }: Props) {
-  return <div className={isOpened ? s.rootVisible : s.rootUnVisible}></div>;
+  const dispatch = useAppDispatch();
+  const handlePackSelect = useCallback(
+    (pack: EPack) => {
+      dispatch(soundEffectsActions.selectPack(pack));
+    },
+    [dispatch]
+  );
+  const config = useAppSelector((store) => store.soundEffectsReducer);
+  return (
+    <div className={isOpened ? s.rootVisible : s.rootUnVisible}>
+      <ul>
+        <Select
+          value={config.pack}
+          onChange={handlePackSelect}
+          options={Object.values(EPack).map((value) => ({
+            value,
+            label: value,
+          }))}
+        />
+      </ul>
+    </div>
+  );
 }
