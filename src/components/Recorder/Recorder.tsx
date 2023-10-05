b8e75bbcf948f9381d9066b725e129627124ba78
Alexey Grevtsov <grevold@yandex.ru>
2023-10-05 23:10:41 +0300

    Отредактировал вид перключателя

---
+++ src/components/Recorder/Recorder.tsx
@@ -0,0 +1,7 @@
+import { ReactMediaRecorder } from "react-media-recorder";
+import * as Tone from "tone";
+
+export function Recorder() {
+  const recorder = new Tone.Recorder();
+  return <div></div>;
+}
