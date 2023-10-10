0349b73e651c6c117a0ceb701925f118764ae1be
Alexey Grevtsov <grevold@yandex.ru>
2023-10-10 16:52:02 +0300

    Добавил адаптив 1

--- src/components/Recorder/Recorder.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactMediaRecorder } from "react-media-recorder";
-import * as Tone from "tone";
-
-export function Recorder() {
-  const recorder = new Tone.Recorder();
-  return <div></div>;
-}
